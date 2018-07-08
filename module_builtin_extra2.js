2c3309543acd61878565919364ec629f355ad917
masashi.g <masashi.g@gmail.com>
2018-07-08 22:25:18 +0900

    fix bugs in bootstrap

---
+++ module_builtin_extra2.js
@@ -0,0 +1,31 @@
+// -------------------------
+// module_println.js - Node.js by Node.js builtin
+// Step10-module:
+// - callBuiltinByName
+// -------------------------
+
+'use strict'
+
+const loadAndParseSrc = require('./module_parser_extra2.js');
+const println = require('./module_println.js');
+const printObj = require('./module_printobj.js');
+const abort = require('./module_abort.js');
+
+// === exports ===
+module.exports = callBuiltinByName;
+
+function callBuiltinByName(name, args) {
+  //const func = eval(name); // OK
+  const func = builtins[name]; // OK
+
+  return func.apply({}, args); // 1st:this, 2nd:args
+}
+
+let builtins = {
+  'require' : require,
+  'println' : println,
+  'printObj' : printObj,
+  'abort' : abort,
+  'callBuiltinByName' : callBuiltinByName,
+  'loadAndParseSrc' :loadAndParseSrc,
+};
---
+++ test_extra2.js
@@ -0,0 +1,13 @@
+! 1;
+1 || 0;
+1 && 0;
+
+let a = true;
+println(a); // true
+println(! a); // false
+println(a || false); // true
+println(a && false); //false 
+
+println(a !== (1 > 0)) //false
+println(a == 1) //true
+println(a === 1) //false
